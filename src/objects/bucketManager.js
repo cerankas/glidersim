5c4dc65ab9f88af4b5c69a262a2b2825deb3595d
cerankas <cerankas@gmail.com>
2026-08-09 06:33:50 +0200

    add bucket manager, improve house/tree generation

---
+++ src/objects/bucketManager.js
@@ -0,0 +1,54 @@
+export class BucketManager {
+  constructor(x, y, tileSize, bucketSize, maxRadius) {
+    this.tileSize = tileSize;
+    this.bucketSize = bucketSize;
+    this.bucketsPerSide = (this.tileSize / this.bucketSize) | 0;
+    this.maxRadius = maxRadius;
+
+    this.buckets = [];
+    for (let i = 0; i < this.bucketsPerSide ** 2; i++) {
+      this.buckets.push([]);
+    }
+
+    this.reset(x, y)
+  }
+
+  bucket(i, j) { return this.buckets[i + this.bucketsPerSide * j]; }
+
+  reset(x, y) {
+    this.x = x;
+    this.y = y;
+
+    for (const bucket of this.buckets) {
+      bucket.length = 0;
+    }
+  }
+
+  add(entity) {
+    const x = entity.x;
+    const y = entity.y;
+    const r = entity.r;
+
+    const { tileSize, bucketSize, bucketsPerSide, maxRadius } = this;
+
+    const i0 = Math.floor((x - this.x + tileSize / 2) / bucketSize);
+    const j0 = Math.floor((y - this.y + tileSize / 2) / bucketSize);
+
+    for (let i = i0 - 1; i <= i0 + 1; i++) {
+      if (i < 0 || i >= bucketsPerSide) continue;
+      if (Math.abs(x - this.x - i * bucketSize) - tileSize / 2 > r + maxRadius) continue;
+
+      for (let j = j0 - 1; j <= j0 + 1; j++) {
+        if (j < 0 || j >= bucketsPerSide) continue;
+        if (Math.abs(y - this.y - j * bucketSize) - tileSize / 2 > r + maxRadius) continue;
+
+        for (const e of this.bucket(i, j)) {
+          if (Math.hypot(x - e.x, y - e.y) < r + e.r) return false;
+        }
+      }
+    }
+
+    this.bucket(i0, j0).push(entity);
+    return true;
+  }
+}
---
+++ src/objects/placeEntities.js
@@ -0,0 +1,42 @@
+import * as THREE from 'three/webgpu';
+import { seededRandom } from '@/utils/random';
+import { terrainHeight } from '@/map/terrain';
+
+
+const vertical = new THREE.Vector3(0,0,1);
+
+
+export function placeEntities(mesh, r, bucketManager, terrain, maxCount) {
+  const size = bucketManager.tileSize;
+  const x0 = bucketManager.x - size / 2;
+  const y0 = bucketManager.y - size / 2;
+  const hash = x0 + (1<<16) * size * y0 + r;
+  const rnd = seededRandom(hash);
+
+  const matrix = new THREE.Matrix4;
+
+  let i = 0;
+  while (maxCount--) {
+    const x = x0 + rnd() * size;
+    const y = y0 + rnd() * size;
+    const z = terrainHeight(x, y);
+
+    if (z < 0) continue;
+
+    const normal = terrain.getNormal(x, y);
+    if (normal == null) continue;
+
+    const angle = normal.angleTo(vertical) * 180 / Math.PI;
+    if (angle > rnd() * 30) continue;
+
+    if (!bucketManager.add({x, y, r})) continue;
+
+    matrix.makeRotationZ(rnd() * 2 * Math.PI);
+    matrix.setPosition(x, y, z);
+    mesh.setMatrixAt(i++, matrix);
+  }
+  mesh.count = i;
+  mesh.instanceMatrix.needsUpdate = true;
+  mesh.computeBoundingBox();
+  mesh.computeBoundingSphere();
+}
