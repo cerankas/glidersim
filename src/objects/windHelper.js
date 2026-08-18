abf6fa5996c9f9484de50573ed87abe952737d13
cerankas <cerankas@gmail.com>
2026-08-18 19:28:35 +0200

    WindHelper with LineSegments

--- src/objects/windHelper.js
+++ src/objects/windHelper.js
@@ -6,17 +6,33 @@ export class WindHelper {
     this.range = range;
     this.count = count;
     this.step = range / count;
-    this.arrows = [];
 
-    for (let i = 0; i < count * count; i++) {
-      const arrow = new THREE.ArrowHelper();
-      this.arrows.push(arrow);
-      scene.add(arrow);
-    }
+    const vertexCount = count * count * 2;
+    this.positions = new Float32Array(vertexCount * 3);
+    this.colors = new Float32Array(vertexCount * 3);
+
+    this.geometry = new THREE.BufferGeometry();
+    this.geometry.setAttribute(
+      'position',
+      new THREE.BufferAttribute(this.positions, 3).setUsage(THREE.DynamicDrawUsage),
+    );
+    this.geometry.setAttribute(
+      'color',
+      new THREE.BufferAttribute(this.colors, 3).setUsage(THREE.DynamicDrawUsage),
+    );
+
+    this.lines = new THREE.LineSegments(
+      this.geometry,
+      new THREE.LineBasicMaterial({ vertexColors: true }),
+    );
+    this.lines.frustumCulled = false;
+    scene.add(this.lines);
   }
 
   update(position, wind, worldManager) {
     const v = position.clone();
+    const positiveColor = new THREE.Color('green');
+    const negativeColor = new THREE.Color('red');
     
     for (let i = 0; i < this.count; i++) {
       v.x = position.x - this.range / 2 + i * this.step;
@@ -25,13 +41,21 @@ export class WindHelper {
         v.y = position.y - this.range / 2 + j * this.step;
       
         const lift = wind.calculateLift(v, worldManager);
+        const offset = (i * this.count + j) * 6;
+        const color = lift > 0 ? positiveColor : negativeColor;
 
-        const arrow = this.arrows[i * this.count + j];
-        arrow.position.set(v.x, v.y, position.z);
-        arrow.setLength(lift);
-        arrow.setColor(lift > 0 ? 'green' : 'red');
-
+        this.positions.set([
+          v.x, v.y, position.z,
+          v.x, v.y, position.z + lift,
+        ], offset);
+        this.colors.set([
+          color.r, color.g, color.b,
+          color.r, color.g, color.b,
+        ], offset);
       }
     }
+
+    this.geometry.attributes.position.needsUpdate = true;
+    this.geometry.attributes.color.needsUpdate = true;
   }
 }
